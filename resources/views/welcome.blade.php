b7490bd16e40f1560e014359449d49f62c35ae7b
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-05-16 00:14:09 +0200

    Change starting page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello
                 </div>
 
                 <div class="links">

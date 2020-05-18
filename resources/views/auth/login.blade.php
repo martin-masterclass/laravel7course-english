9e2f3731c72be1d6f3df9cbe4844c09454de1f18
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-05-18 17:06:43 +0200

    Install Font Awesome

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,7 +54,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    <i class="fas fa-caret-square-right"></i> {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))

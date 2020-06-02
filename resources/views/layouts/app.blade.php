3637beb0e32d1c5142b5ab8bd3efa5cfe508dd5a
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-06-02 17:29:56 +0200

    Validation: Error of all fields

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,17 @@
         </nav>
 
         <main class="py-4">
+            @if($errors->any())
+                <div class="container">
+                    <div class="alert alert-danger" role="alert">
+                        <ul class="mb-0">
+                            @foreach($errors->all() as $error)
+                                <li>{!! $error !!}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            @endif
             @yield('content')
         </main>
     </div>

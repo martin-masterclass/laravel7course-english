8855bd0c98217474a7e2e7ed8b433bde0450c422
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-06-03 23:12:04 +0200

    Edit records with edit()

--- app/Http/Controllers/HobbyController.php
+++ app/Http/Controllers/HobbyController.php
@@ -79,7 +79,9 @@ class HobbyController extends Controller
      */
     public function edit(Hobby $hobby)
     {
-        //
+        return view('hobby.edit')->with([
+            'hobby' => $hobby
+        ]);
     }
 
     /**
---
+++ resources/views/hobby/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Edit Hobby</div>
+                    <div class="card-body">
+                        <form action="/hobby" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" class="form-control{{ $errors->has('name') ? ' border-danger' : '' }}" id="name" name="name" value="{{$hobby->name ?? old('name')}}">
+                                <small class="form-text text-danger">{!! $errors->first('name') !!}</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="description">Description</label>
+                                <textarea class="form-control{{ $errors->has('description') ? ' border-danger' : '' }}" id="description" name="description" rows="5">{{$hobby->description ?? old('description')}}</textarea>
+                                <small class="form-text text-danger">{!! $errors->first('description') !!}</small>
+                            </div>
+                            <input class="btn btn-primary mt-4" type="submit" value="Save Hobby">
+                        </form>
+                        <a class="btn btn-primary float-right" href="/hobby"><i class="fas fa-arrow-circle-up"></i> Back</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

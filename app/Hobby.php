86bfc29858fa0335c58a3f4a391af5f18198bbe0
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-06-22 00:21:08 +0200

    Link models with Eloquent Relationships

--- app/Hobby.php
+++ app/Hobby.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hobby extends Model
 {
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function tags() {
+        return $this->belongsToMany('App\Tag');
+    }
+
+
     /**
      * The attributes that are mass assignable.
      *
--- app/Tag.php
+++ app/Tag.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+
+    public function hobbies() {
+        return $this->belongsToMany('App\Hobby');
+    }
+
     /**
      * The attributes that are mass assignable.
      *

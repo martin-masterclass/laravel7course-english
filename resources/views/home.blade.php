14c8324920ddfdc3a18071d973e518f6e4f44836
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-07-08 17:56:42 +0200

    small correction

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-11">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -14,6 +14,35 @@
                             {{ session('status') }}
                         </div>
                     @endif
+
+                    @isset($hobbies)
+                        @if($hobbies->count() > 0)
+                        <h3>Your Hobbies:</h3>
+                        @endif
+                    <ul class="list-group">
+                        @foreach($hobbies as $hobby)
+                            <li class="list-group-item">
+                                <a title="Show Details" href="/hobby/{{ $hobby->id }}">{{ $hobby->name }}</a>
+                                @auth
+                                    <a class="btn btn-sm btn-light ml-2" href="/hobby/{{ $hobby->id }}/edit"><i class="fas fa-edit"></i> Edit Hobby</a>
+                                @endauth
+
+                                @auth
+                                    <form class="float-right" style="display: inline" action="/hobby/{{ $hobby->id }}" method="post">
+                                        @csrf
+                                        @method("DELETE")
+                                        <input class="btn btn-sm btn-outline-danger" type="submit" value="Delete">
+                                    </form>
+                                @endauth
+                                <span class="float-right mx-2">{{ $hobby->created_at->diffForHumans() }}</span>
+                                <br>
+                                @foreach($hobby->tags as $tag)
+                                    <a href="/hobby/tag/{{ $tag->id }}"><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
+                                @endforeach
+                            </li>
+                        @endforeach
+                    </ul>
+                    @endisset
 
                     <a class="btn btn-success btn-sm" href="/hobby/create"><i class="fas fa-plus-circle"></i> Create new Hobby</a>
                 </div>

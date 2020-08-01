d638047a5f6f8a2b5c2fcc747867fde1484f64e1
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-08-01 23:14:36 +0200

    Apply Auth() middleware to the rest of the controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['show']);
+    }
+
     /**
      * Display a listing of the resource.
      *

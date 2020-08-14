627470157d676bc3ebaf8a5eb97c9a493a8edcbc
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-08-14 13:22:24 +0200

    Create admin middleware and use it for the tags

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::user() && auth()->user()->role !== 'admin') {
+            abort(403, "Only for Admins.");
+        }
+
+        return $next($request);
+    }
+}

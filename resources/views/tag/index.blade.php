ddb675142009a90b13e8818f0da92843b99d86b1
Martin Krebs Eberth <mkrebseberth@gmail.com>
2020-08-27 11:10:35 +0200

    Control buttons in the frontend with @can

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -13,18 +13,25 @@
                             @foreach($tags as $tag)
                                 <li class="list-group-item">
                                     <span style="font-size: 130%;" class="mr-2 badge badge-{{ $tag->style }}">{{ $tag->name }}</span>
-
+                                    @can('update', $tag)
                                     <a class="ml-2 btn btn-sm btn-outline-primary" href="/tag/{{ $tag->id }}/edit"><i class="fas fa-edit"></i> Edit</a>
-                                    <form style="display: inline;" action="/tag/{{ $tag->id }}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <input class="btn btn-outline-danger btn-sm ml-2" type="submit" value="Delete">
-                                    </form>
+                                    @endcan
+
+                                    @can('delete', $tag)
+                                        <form style="display: inline;" action="/tag/{{ $tag->id }}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <input class="btn btn-outline-danger btn-sm ml-2" type="submit" value="Delete">
+                                        </form>
+                                    @endcan
                                     <a class="float-right" href="/hobby/tag/{{ $tag->id }}">Used {{ $tag->hobbies->count() }} times</a>
                                 </li>
                             @endforeach
                         </ul>
-                        <a class="btn btn-success btn-sm mt-3" href="/tag/create"><i class="fas fa-plus-circle"></i> New Tag</a>
+                        @can('create', $tag)
+                            <a class="btn btn-success btn-sm mt-3" href="/tag/create"><i class="fas fa-plus-circle"></i> New Tag</a>
+                        @endcan
+
                     </div>
                 </div>
             </div>
